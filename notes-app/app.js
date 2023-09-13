f2dd04f4d447a8d189d870bb09e1cbda52f14de0
Guy0851 <135039493+Guy0851@users.noreply.github.com>
2023-09-13 14:13:31 +0200

    les colorTags ont maintenant une class

--- notes-app/app.js
+++ notes-app/app.js
@@ -87,7 +87,7 @@ const displayNotes = () => {
         }
 
         document.querySelector('#allNotes').innerHTML += `
-        <div class="listItem" id="tag${tagsArr}">
+        <div class="listItem tag${tagsArr}">
             <div class="headerNote"><h2 class="title">${allNotes[i].title}</h2><button id="${i.toString()}"><svg class="icon" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg></button></div>
             <div class="tagsList">${tagsArr}</div>
             <div class="note">${allNotes[i].note.slice(0,150) + '...'}</div>
