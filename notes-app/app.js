e19f99beaf125e13ccf14cca0132af571458a07b
Guillaume <135039493+Guy0851@users.noreply.github.com>
2023-09-13 16:55:44 +0200

    'see more' button is ready

--- notes-app/app.js
+++ notes-app/app.js
@@ -90,18 +90,24 @@ const displayNotes = () => {
         <div class="listItem tag${tagsArr}">
             <div class="headerNote"><h2 class="title">${allNotes[i].title}</h2><button id="${i.toString()}"><svg class="icon" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg></button></div>
             <div class="tagsList">${tagsArr}</div>
-            <div class="note">${allNotes[i].note.slice(0,150) + '...'}</div>
+            <div class="note"><a id='noteContent${i}'>${allNotes[i].note.slice(0,150) + '...'}</a></div>
             <div class="timestamp">${allNotes[i].created}</div>
+            <div align='center'><button class='seeMore' id='seeMore${i}'>See more</button></div>
         </div>`
-    }
 
+        //si la note fait - de 150 caractères, pas de bouton see more
+        if (allNotes[i].note.length<150){
+            let lop = document.getElementById(`seeMore${i}`)
+            lop.parentNode.removeChild(lop)
+        }
+    }
+    
     allNotesList.style.display = 'inherit'
     newNoteBtn.style.display = 'inherit'
-
+    
 
     localStorage.setItem('allNotes', JSON.stringify(allNotes))
 }
-
 displayNotes()
 
 for (let i = 0 ; i < allNotes.length ; i++) {
@@ -110,9 +116,20 @@ for (let i = 0 ; i < allNotes.length ; i++) {
         const note = allNotes.splice(i, 1)
         archives.push(note)
         displayNotes()
-
+        
         if (allNotes.length == 0) {
     noNotes.innerHTML += "<p> no notes yet </p>"
-}
-    })
+}})}
+
+//bouton see more qui affiche le rest de la note de plus de 150 caractères
+for (let i = 0 ; i < allNotes.length ; i++) {
+    const noteToModify = document.getElementById(`seeMore${i}`)
+    noteToModify.addEventListener("click", (event) => {
+        let noteContent = document.getElementById(`noteContent${i}`);
+        let newNoteContent = document.createTextNode(`${allNotes[i].note}`);
+        
+    //remplacer les 150 premiers caractères par la note en entière
+        noteContent.replaceChild(newNoteContent, noteContent.firstChild);
+    //retire le bouton "see more" après avoir cliqué dessus.
+        noteToModify.parentNode.removeChild(noteToModify);})
 }
